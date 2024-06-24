0290eb6d592c5a14565a9ebca0c3fa3359febe40
Larissa Carrara <100985828+Lari-C@users.noreply.github.com>
2024-06-24 00:55:01 -0300

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ const enderecoRouter = require('./routes/endereco');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL
+}));
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
